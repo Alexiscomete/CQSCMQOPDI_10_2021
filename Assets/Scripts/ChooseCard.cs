858304cd2ee0038301179205474748027d895ced
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-10-15 16:50:40 +0200

    cards types

---
+++ Assets/Scripts/ChooseCard.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ChooseCard : MonoBehaviour
+{
+    private static string[] colors = {"Blue", "Yellow", "Red", "Green"};
+    private static string[] values = {"0", "1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "Draw", "Reverse", "Skip"};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
