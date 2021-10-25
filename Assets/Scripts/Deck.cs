e29bfdbc5950fd731957f6ca3cd551db5b1912b5
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-10-25 20:07:36 +0200

    bv

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -6,6 +6,7 @@ public class Deck : MonoBehaviour
     public static Deck[] decks = { null, null, null, null };
     public int deckNum;
     public List<FollowCard> fc = new List<FollowCard>();
+    public Vector3 velocity;
 
     void Start()
     {
@@ -25,12 +26,21 @@ public class Deck : MonoBehaviour
 
     public void SetPosCards()
     {
-        foreach (FollowCard r in fc)
+        if (deckNum == 0)
         {
-            r.follow = gameObject;
-            if (deckNum == 0)
+            float currentx = 0;
+            foreach (FollowCard r in fc)
             {
+                r.transform.position = Vector3.SmoothDamp(r.transform.position, transform.position + new Vector3(currentx, 0, 0), ref velocity, 0.5f);
                 r.SetFace(true);
+                currentx += 0.5f;
+            }
+        }
+        else
+        {
+            foreach (FollowCard r in fc)
+            {
+                r.follow = gameObject;
             }
         }
     }
