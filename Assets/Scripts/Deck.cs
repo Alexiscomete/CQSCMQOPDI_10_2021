f7d76e13c3d0e61561f8e68c75c9f8b915734575
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-10-26 13:19:51 +0200

    wvdxvvdw

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -6,8 +6,6 @@ public class Deck : MonoBehaviour
     public static Deck[] decks = { null, null, null, null };
     public int deckNum;
     public List<FollowCard> fc = new List<FollowCard>();
-    public Vector3 velocity;
-
     void Start()
     {
         decks[deckNum] = this;
@@ -31,7 +29,8 @@ public class Deck : MonoBehaviour
             float currentx = 0;
             foreach (FollowCard r in fc)
             {
-                r.transform.position = Vector3.SmoothDamp(r.transform.position, transform.position + new Vector3(currentx, 0, 0), ref velocity, 0.5f);
+                r.target = transform.position + new Vector3(currentx, -1.3f, fc.Count - currentx*2);
+                r.asTarget = true;
                 r.SetFace(true);
                 currentx += 0.5f;
             }
--- Assets/Scripts/FollowCard.cs
+++ Assets/Scripts/FollowCard.cs
@@ -12,6 +12,8 @@ public class FollowCard : MonoBehaviour
     public SpriteRenderer spBack;
     public ChooseCard faceOfCard;
     public BoxCollider2D minCollider, maxCollider;
+    public bool asTarget = false;
+    public Vector3 target;
 
     void Start()
     {
@@ -33,6 +35,17 @@ public class FollowCard : MonoBehaviour
         {
             transform.position = Vector3.SmoothDamp(transform.position, follow.transform.position, ref velocity, timeOffSet);
         }
+        else if (asTarget)
+        {
+            if (Distance(target) > 0.03)
+            {
+                transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, 0.5f);
+            }
+            else
+            {
+                asTarget = false;
+            }
+        }
     }
 
     public void SetFace(bool face)
@@ -48,5 +61,10 @@ public class FollowCard : MonoBehaviour
             spFront.color = new Color(255, 255, 255, 0);
             spBack.color = new Color(255, 255, 255, 255);
         }
+    }
+
+    float Distance(Vector2 vec)
+    {
+        return Mathf.Sqrt(Mathf.Pow(vec.x - transform.position.x, 2) + Mathf.Pow(vec.y - transform.position.y, 2));
     }
 }
